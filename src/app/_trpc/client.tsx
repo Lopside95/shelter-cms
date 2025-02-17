c2b242fe30a698c32bf30b3601590c609c5e09c6
James Wallington <jameswallington11@gmail.com>
2025-02-17 17:04:32 +0000

    move landing page

--- src/app/_trpc/client.tsx
+++ src/app/_trpc/client.tsx
@@ -54,18 +54,3 @@ export function TRPCProvider(
     </trpc.Provider>
   );
 }
-
-// // import { createTRPCReact } from "@trpc/react-query";
-
-// import { type AppRouter } from "@/server";
-// import { createTRPCClient, httpBatchLink } from "@trpc/client";
-
-// export const trpc = createTRPCClient<AppRouter>({
-//   links: [
-//     httpBatchLink({
-//       url: "http://localhost:3000/api/trpc",
-//     }),
-//   ],
-// });
-
-// // export const trpc = createTRPCReact<AppRouter>({});
