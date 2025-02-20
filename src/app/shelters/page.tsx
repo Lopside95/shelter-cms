8c647d8d31c9fcdae0abb1feab7129c0fbbcf309
James Wallington <jameswallington11@gmail.com>
2025-02-20 09:58:55 +0000

    add shelter by name working

--- src/app/shelters/page.tsx
+++ src/app/shelters/page.tsx
@@ -6,6 +6,7 @@ import { Shelter } from "@/utils/types";
 import { useEffect, useRef, useState } from "react";
 import { api, TRPCProvider } from "../trpc/client";
 import { get } from "http";
+import { Form } from "react-hook-form";
 
 const Home = () => {
   const [shelters, setShelters] = useState<Shelter[] | null>(null);
@@ -22,22 +23,19 @@ const Home = () => {
 
   const utils = api.useUtils();
 
-  // useEffect(() => {
-  //   if (shelt?.length) {
-  //     setShelters(shelt as Shelter[]);
-  //   }
-  // }, [shelt]);
-
-  if (!shelters) {
-    return <h1>Loading Shelters...</h1>;
-  }
-
   return (
-    <TRPCProvider>
-      <main className="flex flex-col items-center align-middle ">
-        <h1>Welcome to the Shelters page</h1>
-      </main>
-    </TRPCProvider>
+    <main className="flex flex-col items-center align-middle ">
+      <h1>Welcome to the Shelters page</h1>
+      <Input className="w-80" onChange={(e) => setToSearch(e.target.value)} />
+      <Button
+        onClick={() => {
+          singleShelter.refetch();
+          console.log(singleShelter);
+        }}
+      >
+        Get shelter by name
+      </Button>
+    </main>
   );
 };
 
